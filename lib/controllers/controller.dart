a0c3fd35cb505ae29b82e319226a5cdad768843a
bshaymagdy outlook.com <bshaymagdy@outlook.com>
2026-07-18 18:48:58 +0300

    add function start from select choce from select function use

--- lib/controllers/controller.dart
+++ lib/controllers/controller.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:hospital_system/models/patient_model.dart';
 
 class Controller {
@@ -22,7 +24,10 @@ class Controller {
   }
 
   void showPatients() {
-    for (int i = 1; i < 20; i++) {
+    if (patients.every((element) => element.isEmpty)) {
+      print('no patients at the moment');
+    }
+    for (int i = 1; i < patients.length; i++) {
       if (patients[i].isNotEmpty) {
         print("there are ${patients[i].length} patients in Specialization $i");
         for (int j = 0; j < patients[i].length; j++) {
@@ -35,10 +40,69 @@ class Controller {
   }
 
   void getNextpatient({required int specialization}) {
+    if (specialization < 1 || specialization > 20) {
+      throw Exception("Specialization must be between 1 and 20");
+    }
     if (patients[specialization].isEmpty) {
       print('no patients at the moment. have rest, Dr');
     } else {
-      print("${patients[specialization].removeAt(0).name} please come to the doctor room");
+      print(
+        "${patients[specialization].removeAt(0).name} please come to the doctor room",
+      );
+    }
+  }
+
+  void start() {
+    while (true) {
+      print('1. Add new patient');
+      print('2. Show all patients');
+      print('3. Get next patient');
+      print('4. Exit');
+
+      int? choice = int.tryParse(stdin.readLineSync()!);
+
+      if (choice == null) {
+        print("Invalid input");
+        continue;
+      }
+
+      if (choice == 4) {
+        print("Goodbye!");
+        break;
+      }
+
+      switch (choice) {
+        case 1:
+          print('Enter Patient Name:');
+          String name = stdin.readLineSync()!;
+
+          print('Enter Patient Status:');
+          int status = int.parse(stdin.readLineSync()!);
+
+          print('Enter Patient Specialization:');
+          int specialization = int.parse(stdin.readLineSync()!);
+
+          addPatient(
+            name: name,
+            status: status,
+            specialization: specialization,
+          );
+          break;
+
+        case 2:
+          showPatients();
+          break;
+
+        case 3:
+          print('Enter Patient Specialization:');
+          int specialization = int.parse(stdin.readLineSync()!);
+
+          getNextpatient(specialization: specialization);
+          break;
+
+        default:
+          print('Invalid Choice');
+      }
     }
   }
 }
